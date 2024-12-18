4150e184d0ba77e3e8e425dd658e8697da235fc4
Zumbaiero <110639904+Zumbaiero@users.noreply.github.com>
2024-12-18 21:33:55 +0000

    adicionando scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para praticar programação usando o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Eduarda, eu te amo. Se você me ama, dá uma risadinha =D</h1>
+function Home() {
+  return <h1>Eduarda, eu te amo. Se você me ama, dá uma risadinha =D</h1>;
 }
 
 export default Home;
